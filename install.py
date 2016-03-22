5a278a39962896a82de77c759566a089f24deb48
Bernd Schwarzenbacher <bernd.schwb@yahoo.com>
2016-03-22 06:04:13 +0100

    Change imports in install.py to Jupyter4.x style

--- install.py
+++ install.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 from os import path
-from IPython.html.nbextensions import install_nbextension
-from IPython.html.services.config import ConfigManager
+
+from notebook.nbextensions import install_nbextension
+from notebook.services.config import ConfigManager
 
 install_nbextension(
     path.join(path.dirname(path.abspath(__file__)), 'notebook_input_mode'), user=True, verbose=2)
